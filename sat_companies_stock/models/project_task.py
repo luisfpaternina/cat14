1bcda670bba1835ae0c1cc9518c86635f2613b63
Luis Felipe Paternina <lfpaternina93@gmail.com>
2022-08-03 05:54:45 -0500

    agregar stock

---
+++ sat_companies_stock/models/project_task.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+from datetime import datetime, date
+import logging
+
+
+class ProjectTaks(models.Model):
+    _inherit = 'project.task'
+
+    product_id = fields.Many2one(
+        'product.template',
+        string="Gadget")
+    is_conflictivea_pparatus = fields.Boolean(
+        string="Is conflictive apparatus")
+
+    """
+    @api.onchange('product_id')
+    def onchange_recurring_values(self):
+        for record in self:
+            if record.product_id and record.partner_id:
+                    product = record.product_id
+                    if product.is_recurring_task:
+                        record._compute_repeat()
+    """
+
+    def _compute_repeat(self):
+        print('test')
+        res = super(ProjectTaks, self)._compute_repeat()
+        if self:
+            product = self.product_id
+            if product.is_recurring_task:
+                self.recurring_task = product.is_recurring_task
+                self.repeat_interval = product.repeat_interval
+                self.repeat_unit = product.repeat_unit
+                self.repeat_until = product.repeat_until
+                self.repeat_number = product.repeat_number
+                self.repeat_type = product.repeat_type
+                self.mon = product.mon
+                self.tue = product.tue
+                self.wed = product.wed
+                self.thu = product.thu
+                self.fri = product.fri
+                self.sat = product.sat
+                self.sun = product.sun
+            else:
+                self.recurring_task = False
+                self.repeat_interval = False
+                self.repeat_unit = False
+                self.repeat_type = False
+                self.mon = False
+                self.tue = False
+                self.wed = False
+                self.thu = False
+                self.fri = False
+                self.sat = False
+                self.sun = False
+
+        return self
