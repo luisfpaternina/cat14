93c68e1a161199fa784ca2baf714e304ee14c7d8
Luis Felipe Paternina <53566346+luisfpaternina@users.noreply.github.com>
2022-08-03 05:35:53 -0500

    Add files via upload

---
+++ gt_vendor_advance_payment/models/account_move.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    origin_po = fields.Char(
+        string="Origen")
