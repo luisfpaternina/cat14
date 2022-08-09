c452dadd2eab768adc83f1b5c34a58b2aad36401
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-08-09 04:59:10 -0500

    add

--- school/models/cancel_license_plates.py
+++ school/models/cancel_license_plates.py
@@ -12,119 +12,17 @@
 #
 ###################################################################################################################################
 from odoo import models, fields, api, _
-from odoo.addons.mail.models import mail_template
 
 
-class LicensePlates(models.Model):
+class CancelLicensePlates(models.Model):
 
-    _name = "license.plates"
+    _name = "cancel.license.plates"
     _inherit = 'mail.thread'
-    _description = "License plates"
+    _description = "Cancel license plates"
 
     name = fields.Char(
         string="Nombre",
-        readonly=True,
-        required=True,
-        copy=False,
-        tracking=True,
-        default='New')
+        tracking=True)
     student_id = fields.Many2one(
         'students',
         string="Student")
-    course = fields.Char(
-        string="Course")
-    create_date = fields.Datetime(
-        'Create Date',
-        tracking=True,
-        default= fields.Datetime().now())
-    identification_number = fields.Char(
-        string="Identification number",
-        tracking=True,
-        related="student_id.identification_number")
-    blood_group = fields.Selection(
-        string="Blood Group",
-        related="student_id.blood_group")
-    identification_type = fields.Selection(
-        string="Identification Type",
-        related="student_id.identification_type")
-    address = fields.Char(
-        string="Address",
-        related="student_id.address")
-    cellphone = fields.Char(
-        string="Phone",
-        related="student_id.cellphone")
-    neighborhood = fields.Char(
-        string="Neighborhood",
-        related="student_id.neighborhood")
-    gender = fields.Selection(
-        string="Gender",
-        related="student_id.gender")
-    email = fields.Char(
-        string="Email",
-        related="student_id.email")
-    course_line_ids = fields.One2many(
-        'license.plates.lines',
-        'license_plates_id',
-        string="Courses")
-    notes = fields.Html(
-        string="Notes")
-
-
-    def method_name(self):
-        converted_content = mail_template.mako_template_env.from_string(notes).render({'object': object_value,})
-        return converted_content
-
-
-    #Secuencia para las matriculas de los estudiantes
-    @api.model
-    def create(self, vals):
-        if vals.get('name', 'New') == 'New':
-            vals['name'] = self.env['ir.sequence'].next_by_code('license.plates') or 'New'
-
-        result = super(LicensePlates, self).create(vals)
-        return result
-
-
-    #crear nuevo registro en sale.order
-    def create_record_in_Sale_order(self):
-
-        vals = {
-
-           'name': self.name,
-           'partner_id': self.student_id.id,
-           'validity_date': self.create_date,
-           
-		}
-        self.env['sale.order'].create(vals)
-
-
-    @api.onchange('student_id')
-    def get_records(self):
-        for rec in self:
-            lines = []
-            student_obj = rec.env['students'].search([('id', '=', rec.student_id.id)],limit=1)
-            if student_obj:
-                for s in student_obj.course_line_ids:
-                    vals = (0, 0, {
-                        'course_id': s.course_id.id,
-                    })
-                    lines.append(vals)
-                rec.course_line_ids = lines
-
-    
-
-
-class LicensePlatesLines(models.Model):
-
-    _name = "license.plates.lines"
-    _description = "License plates lines"
-
-    course_id = fields.Many2one(
-        'courses',
-        string="Course")
-    student_id = fields.Many2one(
-        'students',
-        string="Student")
-    license_plates_id = fields.Many2one(
-        'license.plates',
-        string="License plates")
--- school/wizard/wizard_mark_gadget_stopped.py
+++ school/wizard/wizard_mark_gadget_stopped.py
@@ -5,22 +5,24 @@ from datetime import date, datetime, timedelta
 from odoo import api, fields, models, _
 
 
-class ProdctStoppedWizard(models.TransientModel):
-    _name = "product.stopped.wizard"
-    _description = "Gadget stopped"
+class LicencePlatesWizard(models.TransientModel):
+    _name = "license.plates.wizard"
+    _description = "Licence plates wizard"
 
-    stop_reason_id = fields.Many2one(
-        'gadget.stop.reason',
-        string="Reason")
-    product_id = fields.Many2one(
-        'product.template',
-        string="Gadget")
+    student_id = fields.Many2one(
+        'students',
+        string="Student")
+    license_plates_id = fields.Many2one(
+        'license.plates',
+        string="Licence plates")
+    description = fields.Text(
+        string="Description")
     
 
     def gadget_stopped_confirm(self):
-        product_obj =  self.env['product.template'].search([('id','=',self.product_id.id)])
-        if product_obj:
-            product_obj.write({
+        license_plates_obj =  self.env['license.plates'].search([('id','=',self.license_plates_id.id)])
+        if license_plates_obj:
+            license_plates_obj.write({
                 'is_gadget_stopped': True, 
                 'stop_reason_id': self.stop_reason_id.id
                 })
