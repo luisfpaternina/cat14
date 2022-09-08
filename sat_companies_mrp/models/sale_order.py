d54cf4ead2b975f8369164cf8c66ee2a8e4293df
Luis Felipe Paternina <lfpaternina93@gmail.com>
2022-09-08 04:47:53 -0500

    traducciones sat_comapnies

--- sat_companies_mrp/models/sale_order.py
+++ sat_companies_mrp/models/sale_order.py
@@ -2,8 +2,3 @@ from odoo import models, fields, api, _
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-
-    mrp_bom_line_ids = fields.One2many(
-        'mrp.bom.line',
-        'sale_order_id',
-        string="Bom lines")
